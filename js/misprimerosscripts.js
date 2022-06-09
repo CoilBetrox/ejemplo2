dec01ec6add6fb18cc8e580f424d6dbd4dfad0ae
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2022-06-08 19:41:48 -0500

    Taller

---
+++ js/misprimerosscripts.js
@@ -0,0 +1,22 @@
+function ocultarMostrarImg(){
+    //display puede tener none, block
+    var valorDisplay = document.getElementById('idImagen').style.display;
+    if(valorDisplay == 'none'){
+        valorDisplay = 'block';
+    }else{
+        valorDisplay = 'none';
+    }
+    document.getElementById('idImagen').style.display = valorDisplay;
+}
+
+function enciende(){
+    var valorSrc = document.getElementById('idImagenFoco').scr;
+    console.log(valorSrc);
+    document.getElementById('idImagenFoco').src='../img/apagado.png';
+}
+
+function apaga(){
+    var valorSrc = document.getElementById('idImagenFoco').scr;
+    console.log(valorSrc);
+    document.getElementById('idImagenFoco').src='../img/encendido.png';
+}
